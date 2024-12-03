660431f0e43080018780c1a9f8c969e3f7f80545
HaineSensei <81431922+HaineSensei@users.noreply.github.com>
2024-12-03 17:00:53 +0000

    Update part2.ts
    removed unnecessary sorting

--- Day-1/typescript/src/part2.ts
+++ Day-1/typescript/src/part2.ts
@@ -7,8 +7,8 @@ const data: [number, number][] = fs.readFileSync('../data.txt', 'utf8')
         const nums = line.split('   ').filter(s => s.length > 0).map(Number);
         return [nums[0], nums[1]] as [number, number];
     });
-const data_first: number[] = data.map(a => a[0]).sort();
-const data_second: number[] = data.map(a => a[1]).sort();
+const data_first: number[] = data.map(a => a[0]);
+const data_second: number[] = data.map(a => a[1]);
 let sim_score: number = 0;
 for (const x of data_first) {
     sim_score += x*data_second.filter(y => y === x).length;
