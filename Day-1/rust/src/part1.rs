fa1e89588ba9f9553e5f86c11f7e22ab2b950c1a
HaineSensei <81431922+HaineSensei@users.noreply.github.com>
2024-12-04 17:38:56 +0000

    Added rust solns

--- Day-1/rust/src/part1.rs
+++ Day-1/rust/src/part1.rs
@@ -9,7 +9,19 @@ pub fn solve(input_path: &str) -> Result<i32, Box<dyn std::error::Error>> {
              nums.next().unwrap().parse().unwrap())
         })
         .collect();
+    let mut data_first: Vec<i32> = data_pairs.iter()
+        .map(|(x,_)| *x)
+        .collect::<Vec<_>>();
+    let mut data_second: Vec<i32> = data_pairs.iter()
+        .map(|(_,y)| *y)
+        .collect::<Vec<_>>();
     
+    data_first.sort();
+    data_second.sort();
+    let total: i32 = (0..data_first.len())
+        .map(|i: usize| data_first[i] - data_second[i])
+        .map(i32::abs)
+        .sum();
     // Rest of solution here
-    Ok(0) // Replace with actual result
+    Ok(total) // Replace with actual result
 }
--- Day-1/rust/src/part2.rs
+++ Day-1/rust/src/part2.rs
@@ -9,7 +9,17 @@ pub fn solve(input_path: &str) -> Result<i32, Box<dyn std::error::Error>> {
              nums.next().unwrap().parse().unwrap())
         })
         .collect();
+    let data_first: Vec<i32> = data_pairs.iter()
+        .map(|(x,_)| *x)
+        .collect::<Vec<_>>();
+    let data_second: Vec<i32> = data_pairs.iter()
+        .map(|(_,y)| *y)
+        .collect::<Vec<_>>();
+
+    let sim_score: i32 = data_first.iter()
+        .map(|&x|x * data_second.iter().filter(|&y| x == *y).count() as i32)
+        .sum();
     
     // Rest of solution here
-    Ok(0) // Replace with actual result
+    Ok(sim_score) // Replace with actual result
 }
